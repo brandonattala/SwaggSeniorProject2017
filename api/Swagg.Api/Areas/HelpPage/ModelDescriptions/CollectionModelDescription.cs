13b6841b0a990e2c86361eb79ca19b26b552cbef
Josh Hoiland <hoilandj@mail.gvsu.edu>
2017-02-09 17:17:40 -0500

    Creates Visual Studio Project using ASP.Net Web API template. Closes #8

---
+++ api/Swagg.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Swagg.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
